002b9c34a5fcfb6cf79ed08d6e4c563fe7221683
Jas-96 <30918742+Jas-96@users.noreply.github.com>
2019-03-16 12:36:23 +0530

    Add files via upload

---
+++ Lab3_Ques3.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Ques {
+
+  private static Scanner s;
+  public String rev(int num)
+  {
+    String r="";
+    while(num>0)
+    {
+      r=r+num%10;
+      num=num/10;
+    }
+    return r;
+    
+  }
+  public static void main(String[] args) {
+    System.out.println("Enter number of Elements in Array : ");
+    Ques q= new Ques();
+    s = new Scanner(System.in);
+    int n = s.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=s.nextInt();
+    }
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=Integer.parseInt(q.rev(arr[i]));
+    }
+    for(int i=0;i<n-1;i++)
+    {
+      for(int j=0;j<n-i-1;j++)
+      {
+        if(arr[j]>arr[j+1])
+        {
+          int temp=arr[j];
+          arr[j]=arr[j+1];
+          arr[j+1]=temp;
+        }
+      }
+    }
+	System.out.println("Sorted Element of Array is : ");
+    for(int i=0;i<n;i++)
+    {
+      System.out.println(arr[i]);
+    }
+  }
+
+}
